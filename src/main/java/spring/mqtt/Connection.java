556cfd65eb449a3a865868dc7486569439aa0acb
robertoklein <robertoluizklein@gmail.com>
2017-12-20 18:12:49 -0200

    configurado para rede local

--- src/main/java/spring/mqtt/Connection.java
+++ src/main/java/spring/mqtt/Connection.java
@@ -14,10 +14,17 @@ public class Connection {
 												};
 
 		// ----- DADOS DE CONEXAO ----- 
-		public static final String HOST_NAME = "m14.cloudmqtt.com";
-		public static final int PORT_NUMBER = 19616;
-		public static final String USERNAME = "axxfrycf";
-		public static final String PASSWORD = "qAdD5bpKyIk4";
+		//public static final String HOST_NAME = "m14.cloudmqtt.com";
+		//public static final int PORT_NUMBER = 19616;
+		//public static final String USERNAME = "axxfrycf";
+		//public static final String PASSWORD = "qAdD5bpKyIk4";
+		
+		// ----- DADOS DE CONEXAO LOCAL ----- 
+		public static final String HOST_NAME = "localhost";
+		public static final int PORT_NUMBER = 1883;
+		//public static final String USERNAME = "axxfrycf";
+		//public static final String PASSWORD = "qAdD5bpKyIk4";
+		
 
 		
 		public static MqttClient getClient() throws MqttException {
@@ -31,8 +38,8 @@ public class Connection {
 		public static MqttConnectOptions getOptions() {
 			MqttConnectOptions conOpt = new MqttConnectOptions();
 	        conOpt.setCleanSession(true);
-	        conOpt.setUserName(USERNAME);
-	        conOpt.setPassword(PASSWORD.toCharArray());
+	        //conOpt.setUserName(USERNAME);
+	        //conOpt.setPassword(PASSWORD.toCharArray());
 	        return conOpt;
 		}
 	
